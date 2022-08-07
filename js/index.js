285a5123ff1a4f369baa4997e25275ceb5032d5e
jle52 <100728904+lejennydesign@users.noreply.github.com>
2022-08-06 23:53:45 -0600

    Added jQuery events, minor UI changes
    Added source code for Javascript and jQuery scripts, and mouse enter scripts in the .js file.
    
    Also made some minor UI changes to the home page on the CSS file (mostly increased padding and rounded borders for sections).

---
+++ js/index.js
@@ -0,0 +1,19 @@
+console.log("Your index.js file is loaded correctly!");
+
+$(document).ready(function(){
+  $("#placeholder").mouseenter(function(){
+    alert("There will be an actual thumbnail here soon, promise.");
+  });
+});
+
+$(document).ready(function(){
+    $("#placeholder2").mouseenter(function(){
+      alert("There will be an actual thumbnail here soon, promise.");
+    });
+  });
+
+  $(document).ready(function(){
+    $("#placeholder3").mouseenter(function(){
+      alert("There will be an actual thumbnail here soon, promise.");
+    });
+  });
