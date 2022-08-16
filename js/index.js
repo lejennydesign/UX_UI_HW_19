9490badb80e16290a8a7347324f75c6d4dc8f417
jle52 <100728904+lejennydesign@users.noreply.github.com>
2022-08-15 18:07:42 -0600

    Visual design, jQuery, and more UI interactions
    Updated the visual design, added UI interactions, and other jQuery-related elements.

--- js/index.js
+++ js/index.js
@@ -1,19 +1,40 @@
 console.log("Your index.js file is loaded correctly!");
 
-$(document).ready(function(){
-  $("#placeholder").mouseenter(function(){
-    alert("There will be an actual thumbnail here soon, promise.");
-  });
-});
-
-$(document).ready(function(){
-    $("#placeholder2").mouseenter(function(){
-      alert("There will be an actual thumbnail here soon, promise.");
-    });
-  });
+// Smooth Scrolling - Happens when you press on Portfolio on the homepage
 
   $(document).ready(function(){
-    $("#placeholder3").mouseenter(function(){
-      alert("There will be an actual thumbnail here soon, promise.");
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = casestudy1;
+        });
+      } // End if
     });
+  });
+
+
+  // jQuery Event Listener: Hover
+
+  $(document).ready(function(){
+    $("#helloBanner").hover(function(){
+      alert("Hello!");
+    },
+    function(){
+      alert("This is my jQuery hover demo.");
+    }); 
   });
